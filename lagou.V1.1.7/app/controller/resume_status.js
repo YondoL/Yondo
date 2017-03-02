e7c7135383bb2ab893149c0f9ab62cf87567bee8
Yondol <1628916868@qq.com>
2017-03-02 18:46:43 +0800

    上传模拟拉钩-angular

---
+++ lagou.V1.1.7/app/controller/resume_status.js
@@ -0,0 +1,11 @@
+angular.module("lagou")
+.controller("resume_status",["$scope",function($scope){
+	$scope.main = [{name:'全部',status:true,hear:'.all'},{name:'被查看',status:false,hear:'.checked'},{name:'待沟通',status:false,hear:'.wait'},{name:'面试',status:false,hear:'.face'},{name:'不合适',status:false,hear:'.no'}]
+	var main = $scope.main;
+	$scope.toggle = function(item){
+		for(ite in main){
+			main[ite].status = false;
+		}
+		item.status = !item.status
+	}
+}])
---
+++ lagou.V1.1.7/app/directive/swiper/swiper.js
@@ -0,0 +1,15 @@
+angular.module("lagou")
+.directive("mySwiper",function(){
+    return{
+        controller:"swiperController",
+        templateUrl:"app/directive/swiper/swiper.html",
+        css : [{
+                href : "public/css/swiper-3.3.1.min.css",
+                persist: true//保留CSS
+                // preload: true,
+            },{
+            	href :"public/css/swipper.css"
+            }
+            ]
+    }
+})
